b34e4f440a4e5899ecd64def72b90c44cc611c50
Paco <psalido@gmail.com>
2016-07-31 21:01:55 +0200

    fix reddit spider

--- unasbravas/spiders/reddit_spider.py
+++ unasbravas/spiders/reddit_spider.py
@@ -6,17 +6,21 @@ KEYWORDS = ['patata', 'brava']
 
 class RedditRecipesSpider(scrapy.Spider):
     name = 'redditrecipes'
-    start_urls = ['https://www.reddit.com/r/recipes']
+    
+    start_urls = []
+    
+    for keyword in KEYWORDS:
+        start_urls.append('https://www.reddit.com/r/recipes/search?q=' + keyword + '&restrict_sr=on')
 
     def parse(self, response):
-        for recipe in response.css('p.title > a.title'):
+        for recipe in response.xpath('//header[@class="search-result-header"]/a'):
             recipe_title = recipe.xpath('text()').extract()
             # If title contains any KEYWORD
             if any(word in recipe_title[0].lower() for word in KEYWORDS):
                 url = recipe.xpath('@href').extract()
                 yield scrapy.Request(response.urljoin(url[0]), self.parse_recipe)
 
-        next_page = response.xpath('a[@rel="next"]/@href')
+        next_page = response.xpath('//a[@rel="nofollow next"]/@href')
 
         if next_page:
             url = response.urljoin(next_page[0].extract())
@@ -24,6 +28,6 @@ class RedditRecipesSpider(scrapy.Spider):
 
     def parse_recipe(self, response):
         item = RecipeItem()
-        item['title'] = response.xpath('/p[@class="title"]/a[@class="title"]/text()').extract()
+        item['title'] = response.css("p.title > a.title::text").extract()
         item['link'] = response.url
         yield item
