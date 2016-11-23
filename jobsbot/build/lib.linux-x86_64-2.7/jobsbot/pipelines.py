ebef36102402bbfbcc49c241816b8650803c570a
Paco <psalido@gmail.com>
2016-11-23 17:19:38 +0100

    modifications

---
+++ jobsbot/build/lib.linux-x86_64-2.7/jobsbot/pipelines.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import requests
+
+class JobsbotPipeline(object):
+
+    def process_item(self, item, spider):
+    	title = item['title'][0]
+    	content = item['content'][0]
+    	place = item['header'][0]
+    	url = item['url']
+    	message = '%s \n -----------\n%s\n\n%s\n\n%s' % (title, content, place, url)
+
+        requests.get('https://api.telegram.org/bot279656133:AAHoIJwa8iG7v7-V6i8zCL6YKaYRFqqep_g/sendMessage?chat_id=@trabajofuengirola&disable_web_page_preview=true&text='+message)
