a76e87970bf49c764b409c82fa9740fb68afea7e
Paco <psalido@gmail.com>
2016-12-06 20:48:50 +0100

    add new spiders

--- jobsbot/build/lib.linux-x86_64-2.7/jobsbot/spiders/milanuncios.py
+++ jobsbot/build/lib.linux-x86_64-2.7/jobsbot/spiders/milanuncios.py
@@ -4,9 +4,8 @@ import scrapy
 from jobsbot.items import OfferItem
 
 BASE_URL = 'http://www.milanuncios.com'
-KEYWORDS = ['fuengirola',]
 
-class MilanunciosSpider(scrapy.Spider):
+class MilanunciosFuengirolaSpider(scrapy.Spider):
     name = "milanuncios"
     allowed_domains = ["milanuncios.com"]
     start_urls = ['http://www.milanuncios.com/ofertas-de-empleo-en-malaga/?dias=1&demanda=n']
@@ -16,7 +15,7 @@ class MilanunciosSpider(scrapy.Spider):
             # If title contains any KEYWORD
             oferta_title = oferta.xpath('div[@class="aditem-header"]/div[@class="x4 display-desktop"]/text()').extract()
             oferta_url = oferta.xpath('div[@class="aditem-detail-container" or @class="aditem-detail-image-container"]/div[@class="aditem-detail"]/a[@class="aditem-detail-title"]/@href').extract()
-            if any(word in oferta_title[0].lower() for word in KEYWORDS):
+            if 'fuengirola' in oferta_title[0].lower():
 	            url = oferta_url[0]
 	            yield scrapy.Request(response.urljoin(url), self.parse_oferta)
 
@@ -32,5 +31,62 @@ class MilanunciosSpider(scrapy.Spider):
         item['title'] = response.xpath('//div[@class="pagAnuTituloBox"]/a/text()').extract()
         item['url'] = BASE_URL + response.xpath('//div[@class="pagAnuTituloBox"]/a/@href').extract()[0]
         item['content'] = response.xpath('//p[@class="pagAnuCuerpoAnu"]/text()').extract()
+        item['city'] = 'fuengirola'
         yield item
-           
+
+class MilanunciosMalagaSpider(scrapy.Spider):
+    name = "milanuncios"
+    allowed_domains = ["milanuncios.com"]
+    start_urls = ['http://www.milanuncios.com/ofertas-de-empleo-en-malaga/?dias=1&demanda=n']
+
+    def parse(self, response):
+        for oferta in response.xpath('//div[@class="aditem"]'):
+            # If title contains any KEYWORD
+            oferta_title = oferta.xpath('div[@class="aditem-header"]/div[@class="x4 display-desktop"]/text()').extract()
+            oferta_url = oferta.xpath('div[@class="aditem-detail-container" or @class="aditem-detail-image-container"]/div[@class="aditem-detail"]/a[@class="aditem-detail-title"]/@href').extract()
+            url = oferta_url[0]
+            yield scrapy.Request(response.urljoin(url), self.parse_oferta)
+
+        next_page = response.xpath('//div[@class="adlist-paginator-pagelink adlist-paginator-pageselected"][last()]/a/@href').extract()
+
+        if next_page:
+            url = response.urljoin(next_page[0])
+            yield scrapy.Request(url, self.parse)
+
+    def parse_oferta(self, response):
+        item = OfferItem()
+        item['header'] = response.xpath('//div[@class="pagAnuCatLoc"]/text()').extract()
+        item['title'] = response.xpath('//div[@class="pagAnuTituloBox"]/a/text()').extract()
+        item['url'] = BASE_URL + response.xpath('//div[@class="pagAnuTituloBox"]/a/@href').extract()[0]
+        item['content'] = response.xpath('//p[@class="pagAnuCuerpoAnu"]/text()').extract()
+        item['city'] = 'malaga'
+        yield item
+                      
+
+class MilanunciosAndaluciaSpider(scrapy.Spider):
+    name = "milanuncios"
+    allowed_domains = ["milanuncios.com"]
+    start_urls = ['http://www.milanuncios.com/ofertas-de-empleo-en-andalucia/?dias=1&demanda=n']
+
+    def parse(self, response):
+        for oferta in response.xpath('//div[@class="aditem"]'):
+            # If title contains any KEYWORD
+            oferta_title = oferta.xpath('div[@class="aditem-header"]/div[@class="x4 display-desktop"]/text()').extract()
+            oferta_url = oferta.xpath('div[@class="aditem-detail-container" or @class="aditem-detail-image-container"]/div[@class="aditem-detail"]/a[@class="aditem-detail-title"]/@href').extract()
+            url = oferta_url[0]
+            yield scrapy.Request(response.urljoin(url), self.parse_oferta)
+
+        next_page = response.xpath('//div[@class="adlist-paginator-pagelink adlist-paginator-pageselected"][last()]/a/@href').extract()
+
+        if next_page:
+            url = response.urljoin(next_page[0])
+            yield scrapy.Request(url, self.parse)
+
+    def parse_oferta(self, response):
+        item = OfferItem()
+        item['header'] = response.xpath('//div[@class="pagAnuCatLoc"]/text()').extract()
+        item['title'] = response.xpath('//div[@class="pagAnuTituloBox"]/a/text()').extract()
+        item['url'] = BASE_URL + response.xpath('//div[@class="pagAnuTituloBox"]/a/@href').extract()[0]
+        item['content'] = response.xpath('//p[@class="pagAnuCuerpoAnu"]/text()').extract()
+        item['city'] = 'andalucia'
+        yield item
--- jobsbot/jobsbot/spiders/milanuncios.py
+++ jobsbot/jobsbot/spiders/milanuncios.py
@@ -4,8 +4,9 @@ import scrapy
 from jobsbot.items import OfferItem
 
 BASE_URL = 'http://www.milanuncios.com'
+KEYWORDS = ['fuengirola',]
 
-class MilanunciosFuengirolaSpider(scrapy.Spider):
+class MilanunciosSpider(scrapy.Spider):
     name = "milanuncios"
     allowed_domains = ["milanuncios.com"]
     start_urls = ['http://www.milanuncios.com/ofertas-de-empleo-en-malaga/?dias=1&demanda=n']
@@ -15,9 +16,9 @@ class MilanunciosFuengirolaSpider(scrapy.Spider):
             # If title contains any KEYWORD
             oferta_title = oferta.xpath('div[@class="aditem-header"]/div[@class="x4 display-desktop"]/text()').extract()
             oferta_url = oferta.xpath('div[@class="aditem-detail-container" or @class="aditem-detail-image-container"]/div[@class="aditem-detail"]/a[@class="aditem-detail-title"]/@href').extract()
-            if 'fuengirola' in oferta_title[0].lower():
-	            url = oferta_url[0]
-	            yield scrapy.Request(response.urljoin(url), self.parse_oferta)
+            if any(word in oferta_title[0].lower() for word in KEYWORDS):
+                url = oferta_url[0]
+                yield scrapy.Request(response.urljoin(url), self.parse_oferta)
 
         next_page = response.xpath('//div[@class="adlist-paginator-pagelink adlist-paginator-pageselected"][last()]/a/@href').extract()
 
@@ -32,61 +33,4 @@ class MilanunciosFuengirolaSpider(scrapy.Spider):
         item['url'] = BASE_URL + response.xpath('//div[@class="pagAnuTituloBox"]/a/@href').extract()[0]
         item['content'] = response.xpath('//p[@class="pagAnuCuerpoAnu"]/text()').extract()
         item['city'] = 'fuengirola'
-        yield item
-
-class MilanunciosMalagaSpider(scrapy.Spider):
-    name = "milanuncios"
-    allowed_domains = ["milanuncios.com"]
-    start_urls = ['http://www.milanuncios.com/ofertas-de-empleo-en-malaga/?dias=1&demanda=n']
-
-    def parse(self, response):
-        for oferta in response.xpath('//div[@class="aditem"]'):
-            # If title contains any KEYWORD
-            oferta_title = oferta.xpath('div[@class="aditem-header"]/div[@class="x4 display-desktop"]/text()').extract()
-            oferta_url = oferta.xpath('div[@class="aditem-detail-container" or @class="aditem-detail-image-container"]/div[@class="aditem-detail"]/a[@class="aditem-detail-title"]/@href').extract()
-            url = oferta_url[0]
-            yield scrapy.Request(response.urljoin(url), self.parse_oferta)
-
-        next_page = response.xpath('//div[@class="adlist-paginator-pagelink adlist-paginator-pageselected"][last()]/a/@href').extract()
-
-        if next_page:
-            url = response.urljoin(next_page[0])
-            yield scrapy.Request(url, self.parse)
-
-    def parse_oferta(self, response):
-        item = OfferItem()
-        item['header'] = response.xpath('//div[@class="pagAnuCatLoc"]/text()').extract()
-        item['title'] = response.xpath('//div[@class="pagAnuTituloBox"]/a/text()').extract()
-        item['url'] = BASE_URL + response.xpath('//div[@class="pagAnuTituloBox"]/a/@href').extract()[0]
-        item['content'] = response.xpath('//p[@class="pagAnuCuerpoAnu"]/text()').extract()
-        item['city'] = 'malaga'
-        yield item
-                      
-
-class MilanunciosAndaluciaSpider(scrapy.Spider):
-    name = "milanuncios"
-    allowed_domains = ["milanuncios.com"]
-    start_urls = ['http://www.milanuncios.com/ofertas-de-empleo-en-andalucia/?dias=1&demanda=n']
-
-    def parse(self, response):
-        for oferta in response.xpath('//div[@class="aditem"]'):
-            # If title contains any KEYWORD
-            oferta_title = oferta.xpath('div[@class="aditem-header"]/div[@class="x4 display-desktop"]/text()').extract()
-            oferta_url = oferta.xpath('div[@class="aditem-detail-container" or @class="aditem-detail-image-container"]/div[@class="aditem-detail"]/a[@class="aditem-detail-title"]/@href').extract()
-            url = oferta_url[0]
-            yield scrapy.Request(response.urljoin(url), self.parse_oferta)
-
-        next_page = response.xpath('//div[@class="adlist-paginator-pagelink adlist-paginator-pageselected"][last()]/a/@href').extract()
-
-        if next_page:
-            url = response.urljoin(next_page[0])
-            yield scrapy.Request(url, self.parse)
-
-    def parse_oferta(self, response):
-        item = OfferItem()
-        item['header'] = response.xpath('//div[@class="pagAnuCatLoc"]/text()').extract()
-        item['title'] = response.xpath('//div[@class="pagAnuTituloBox"]/a/text()').extract()
-        item['url'] = BASE_URL + response.xpath('//div[@class="pagAnuTituloBox"]/a/@href').extract()[0]
-        item['content'] = response.xpath('//p[@class="pagAnuCuerpoAnu"]/text()').extract()
-        item['city'] = 'andalucia'
         yield item
